e1d3d213cde1a5e36695c134372eb30fcea51c08
Raphael Lima <raphaellima365@gmail.com>
2024-03-15 01:12:41 -0300

    criando faq

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (botao) {
@@ -11,7 +12,18 @@ document.addEventListener('DOMContentLoaded', function () {
             botao.target.classList.add('shows__tabs__button--is-active')
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreFechaResposta);
+    }
 })
+
+function abreFechaResposta(e) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = e.target.parentNode;
+
+    elementoPai.classList.toggle(classe)
+}
 
 function removeAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]')
